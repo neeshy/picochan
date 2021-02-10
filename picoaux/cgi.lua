e5e1bb08316b1bf0b0ca0a712f192c69067f9a12
neeshy <neeshy@tfwno.gf>
2021-02-10 15:59:11 -0500

    cgi: refactor write_headers and handle encodings in finalize

--- picoaux/cgi.lua
+++ picoaux/cgi.lua
@@ -5,44 +5,42 @@ local brotli = require("picoaux.brotli");
 local zlib = require("picoaux.zlib");
 local cgi = {};
 
-local function write_headers()
-  for k, v in pairs(cgi.headers) do
-    io.write(k, ": ", v, "\r\n");
-  end
-
-  io.write("\r\n");
-end
-
 function cgi.initialize()
   cgi.outputbuf = {};
   cgi.headers = {};
-  cgi.encodings = {};
-  encodings = ENV["HTTP_ACCEPT_ENCODING"];
-  if encodings then
-    local list = string.tokenize(encodings:gsub(" ", ""):gsub(";q=%n-\\.?%n-", ""), ",");
-    for i = 1, #list do
-      cgi.encodings[list[i]] = true;
-    end
-  end
 end
 
 function cgi.finalize()
   local outputbuf = table.concat(cgi.outputbuf);
 
-  if cgi.encodings["br"] then
+  local encodings = {};
+  local accept_encoding = ENV["HTTP_ACCEPT_ENCODING"];
+  if accept_encoding then
+    local list = accept_encoding:gsub(" ", ""):gsub(";q=%n-\\.?%n-", ""):tokenize(",");
+    for i = 1, #list do
+      encodings[list[i]] = true;
+    end
+  end
+
+  if encodings["br"] then
     cgi.headers["Content-Encoding"] = "br";
     outputbuf = brotli.compress(outputbuf, 2, "text");
-  elseif cgi.encodings["gzip"] then
+  elseif encodings["gzip"] then
     cgi.headers["Content-Encoding"] = "gzip";
     outputbuf = zlib.compress(outputbuf, 1, 31, 9);
-  elseif cgi.encodings["deflate"] then
+  elseif encodings["deflate"] then
     cgi.headers["Content-Encoding"] = "deflate";
     outputbuf = zlib.compress(outputbuf, 1, -15, 9);
   else
     cgi.headers["Content-Encoding"] = "identity";
   end
 
-  write_headers();
+  for k, v in pairs(cgi.headers) do
+    io.write(k, ": ", v, "\r\n");
+  end
+
+  io.write("\r\n");
+
   io.write(outputbuf);
   os.exit(0);
 end
