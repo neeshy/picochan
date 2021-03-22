8540a89c2226f82864517d9247b7c07b21b386bc
neeshy <neeshy@tfwno.gf>
2021-03-21 20:02:39 -0400

    picoaux.cgi: remove brotli and zlib support
    This can and should be handled in the reverse proxy/webserver instead.
    Both lighttpd and nginx support compression of CGI pages.

--- picoaux/cgi.lua
+++ picoaux/cgi.lua
@@ -1,8 +1,6 @@
 -- Picochan-specific CGI functions
 
 require("picoaux.stringmisc");
-local brotli = require("picoaux.brotli");
-local zlib = require("picoaux.zlib");
 local cgi = {};
 
 function cgi.initialize()
@@ -11,37 +9,13 @@ function cgi.initialize()
 end
 
 function cgi.finalize()
-  local outputbuf = table.concat(cgi.outputbuf);
-
-  local encodings = {};
-  local accept_encoding = os.getenv("HTTP_ACCEPT_ENCODING");
-  if accept_encoding then
-    local list = accept_encoding:gsub(" ", ""):gsub(";q=%n-\\.?%n-", ""):tokenize(",");
-    for i = 1, #list do
-      encodings[list[i]] = true;
-    end
-  end
-
-  if encodings["br"] then
-    cgi.headers["Content-Encoding"] = "br";
-    outputbuf = brotli.compress(outputbuf, 2, "text");
-  elseif encodings["gzip"] then
-    cgi.headers["Content-Encoding"] = "gzip";
-    outputbuf = zlib.compress(outputbuf, 1, 31, 9);
-  elseif encodings["deflate"] then
-    cgi.headers["Content-Encoding"] = "deflate";
-    outputbuf = zlib.compress(outputbuf, 1, -15, 9);
-  else
-    cgi.headers["Content-Encoding"] = "identity";
-  end
-
   for k, v in pairs(cgi.headers) do
     io.write(k, ": ", v, "\r\n");
   end
 
   io.write("\r\n");
 
-  io.write(outputbuf);
+  io.write(table.concat(cgi.outputbuf));
   os.exit(0);
 end
 
