f8c2e41e335134a0d9d6641a2e2a36fb67881f71
vxiiduu <73044267+vxiiduu@users.noreply.github.com>
2021-05-12 14:18:21 +1000

    Updated installation instructions and fixed a small bug in the json binding.

--- picoaux/cjson.lua
+++ picoaux/cjson.lua
@@ -47,7 +47,7 @@ local function is_array(t)
     local m, c = 0, 0;
 
     for k, _ in pairs(t) do
-        if type(k) ~= "number" or k < 0 or floor(k) ~= k then return false end;
+        if type(k) ~= "number" or k < 0 or math.floor(k) ~= k then return false end;
         m = math.max(m, k);
         c = c + 1;
     end
