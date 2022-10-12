neeshy/picochan

commit 208b1d7ba3f93e78403e816ea485ccd5dbd5cab4

feature: paginate catalog

pico.lua
@@ -673,7 +673,7 @@ function html.renderindex(index_tbl, board)
       html.renderpost(index_tbl[i][j], overboard)
     end
 
-    printf("</div><hr />")
+    printf("</div>")
   end
 end
 
@@ -684,7 +684,6 @@ function html.renderrecent(recent_tbl, board)
     end
     html.renderpost(recent_tbl[i], board == "Overboard", true)
   end
-  printf("<hr />")
 end
 
 function html.brc(title, redheader)
@@ -1032,8 +1031,8 @@ handlers["/Mod"] = function()
   html.list.entry("<a href='/Mod/global/theme'>Change default site theme</a>")
   html.list.entry("<a href='/Mod/global/defaultpostname'>Change default post name</a>")
   html.list.entry("<a href='/Mod/global/defaultboardview'>Change default board view</a>")
-  html.list.entry("<a href='/Mod/global/catalogsize'>Change catalog page size</a>")
-  html.list.entry("<a href='/Mod/global/overboardsize'>Change overboard catalog page size</a>")
+  html.list.entry("<a href='/Mod/global/catalogpagesize'>Change catalog page size</a>")
+  html.list.entry("<a href='/Mod/global/overboardpagesize'>Change overboard catalog page size</a>")
   html.list.entry("<a href='/Mod/global/indexpagesize'>Change index page size</a>")
   html.list.entry("<a href='/Mod/global/indexwindowsize'>Change index window size</a>")
   html.list.entry("<a href='/Mod/global/recentpagesize'>Change recent posts page size</a>")
@@ -1680,18 +1679,37 @@ local function board_header(board_tbl)
   printf("<a class='float-right' href=''>[Update]</a><hr />")
 end
 
-handlers["/(Overboard)/catalog"] = function()
-  overboard_header()
-  html.rendercatalog(pico.board.catalog())
+handlers["/(Overboard)/catalog"] = function(board, page)
+  local overboard = board == "Overboard"
+  local boardval
+  if overboard then
+    overboard_header()
+  else
+    boardval = board
+    board_header(pico.board.tbl(board))
+  end
+
+  page = tonumber(page) or 1
+  if page <= 0 then
+    cgi.headers["Status"] = "404 Not Found"
+    html.error("Page not found", "Page number too low: %s", page)
+  end
+
+  local catalog_tbl, pagecount = pico.board.catalog(boardval, page)
+  if page > pagecount then
+    cgi.headers["Status"] = "404 Not Found"
+    html.error("Page not found", "Page number too high: %s", page)
+  end
+
+  html.rendercatalog(catalog_tbl)
+  printf("<hr />")
+  html.renderpages(string.format("/%s/catalog", board), page, pagecount)
   html.finish()
 end
 
-handlers["/([%l%d]+)/catalog"] = function(board)
-  local board_tbl = pico.board.tbl(board)
-  board_header(board_tbl)
-  html.rendercatalog(pico.board.catalog(board))
-  html.finish()
-end
+handlers["/(Overboard)/catalog/(%d)"] = handlers["/(Overboard)/catalog"]
+handlers["/([%l%d]+)/catalog"] = handlers["/(Overboard)/catalog"]
+handlers["/([%l%d]+)/catalog/(%d)"] = handlers["/(Overboard)/catalog"]
 
 handlers["/(Overboard)/index"] = function(board, page)
   local overboard = board == "Overboard"
@@ -1716,6 +1734,7 @@ handlers["/(Overboard)/index"] = function(board, page)
   end
 
   html.renderindex(index_tbl, board)
+  printf("<hr />")
   html.renderpages(string.format("/%s/index", board), page, pagecount)
   html.finish()
 end
@@ -1747,6 +1766,7 @@ handlers["/(Overboard)/recent"] = function(board, page)
   end
 
   html.renderrecent(recent_tbl, board)
+  printf("<hr />")
   html.renderpages(string.format("/%s/recent", board), page, pagecount)
   html.finish()
 end
@@ -1765,6 +1785,7 @@ elseif defaultboardview == "recent" then
   handlers["/([%l%d]+)/?"] = handlers["/([%l%d]+)/recent"]
 else
   handlers["/(Overboard)"] = handlers["/(Overboard)/catalog"]
+  handlers["/(Overboard)/(%d+)"] = handlers["/(Overboard)/catalog"]
   handlers["/([%l%d]+)/?"] = handlers["/([%l%d]+)/catalog"]
 end
 

picochan.sql
@@ -256,8 +256,8 @@ INSERT INTO GlobalConfig VALUES ('defaultpostname', 'Anonymous');
 INSERT INTO GlobalConfig VALUES ('defaultboardview', 'catalog');
 INSERT INTO GlobalConfig VALUES ('frontpage', 'Welcome to Picochan.');
 INSERT INTO GlobalConfig VALUES ('theme', 'picochan');
-INSERT INTO GlobalConfig VALUES ('catalogsize', 1000);
-INSERT INTO GlobalConfig VALUES ('overboardsize', 100);
+INSERT INTO GlobalConfig VALUES ('catalogpagesize', 1000);
+INSERT INTO GlobalConfig VALUES ('overboardpagesize', 100);
 INSERT INTO GlobalConfig VALUES ('indexpagesize', 10);
 INSERT INTO GlobalConfig VALUES ('indexwindowsize', 5);
 INSERT INTO GlobalConfig VALUES ('recentpagesize', 50);

picoengine.lua
@@ -330,23 +330,36 @@ function pico.board.configure(board_tbl)
   return true, "Board configured successfully"
 end
 
-function pico.board.catalog(name)
+function pico.board.catalog(name, page)
   if name and not pico.board.exists(name) then
     return nil, "Board does not exist"
   end
 
-  local pagesize = pico.global.get(name and "catalogsize" or "overboardsize")
+  page = tonumber(page) or 1
+  local pagesize = pico.global.get(name and "catalogpagesize" or "overboardpagesize")
+
+  local where = name and "Threads.Board = ? "
+                      or "Threads.Board IN (SELECT Name FROM Boards WHERE DisplayOverboard) "
   local sql = "SELECT Threads.*, Posts.*, File, Spoiler, Width AS FileWidth, Height AS FileHeight " ..
               "FROM Threads JOIN Posts USING(Board, Number) LEFT JOIN FileRefs USING(Board, Number) LEFT JOIN Files ON Files.Name = FileRefs.File " ..
-              "WHERE (Sequence = 1 OR Sequence IS NULL) " ..
-              (name and "AND Posts.Board = ? "
-                     or "AND Posts.Board IN (SELECT Name FROM Boards WHERE DisplayOverboard) ") ..
+              "WHERE (Sequence = 1 OR Sequence IS NULL) AND " ..
+              where ..
               "ORDER BY " ..
-              (name and "Sticky DESC, LastBumpDate DESC, Posts.Number DESC "
+              (name and "Sticky DESC, LastBumpDate DESC, Threads.Number DESC "
                      or "LastBumpDate DESC ") ..
-              "LIMIT ?"
-  return name and db:q(sql, name, pagesize)
-               or db:q(sql, pagesize)
+              "LIMIT ? OFFSET ?"
+  local pagecount_sql = "SELECT ((COUNT(*) - 1) / CAST(? AS INTEGER)) + 1 FROM Threads WHERE " .. where
+
+  local catalog_tbl, pagecount
+  if name then
+    catalog_tbl = db:q(sql, name, pagesize, (page - 1) * pagesize)
+    pagecount = db:r1(pagecount_sql, pagesize, name)
+  else
+    catalog_tbl = db:q(sql, pagesize, (page - 1) * pagesize)
+    pagecount = db:r1(pagecount_sql, pagesize)
+  end
+
+  return catalog_tbl, pagecount
 end
 
 function pico.board.index(name, page)
@@ -366,10 +379,14 @@ function pico.board.index(name, page)
               "LastBumpDate DESC LIMIT ? OFFSET ?"
   local pagecount_sql = "SELECT ((COUNT(*) - 1) / CAST(? AS INTEGER)) + 1 FROM Threads " .. where
 
-  local thread_ops = name and db:q(sql, name, pagesize, (page - 1) * pagesize)
-                           or db:q(sql, pagesize, (page - 1) * pagesize)
-  local pagecount = name and db:r1(pagecount_sql, pagesize, name)
-                          or db:r1(pagecount_sql, pagesize)
+  local thread_ops, pagecount
+  if name then
+    thread_ops = db:q(sql, name, pagesize, (page - 1) * pagesize)
+    pagecount = db:r1(pagecount_sql, pagesize, name)
+  else
+    thread_ops = db:q(sql, pagesize, (page - 1) * pagesize)
+    pagecount = db:r1(pagecount_sql, pagesize)
+  end
 
   local index_tbl = {}
   for i = 1, #thread_ops do
@@ -388,6 +405,10 @@ function pico.board.index(name, page)
 end
 
 function pico.board.recent(name, page)
+  if name and not pico.board.exists(name) then
+    return nil, "Board does not exist"
+  end
+
   page = tonumber(page) or 1
   local pagesize = pico.global.get("recentpagesize")
 
@@ -395,10 +416,14 @@ function pico.board.recent(name, page)
   local sql = "SELECT * FROM Posts " ..  where ..  "ORDER BY Date DESC LIMIT ? OFFSET ?"
   local pagecount_sql = "SELECT ((COUNT(*) - 1) / CAST(? AS INTEGER)) + 1 FROM Posts " .. where
 
-  local recent_tbl = name and db:q(sql, name, pagesize, (page - 1) * pagesize)
-                           or db:q(sql, pagesize, (page - 1) * pagesize)
-  local pagecount = name and db:r1(pagecount_sql, pagesize, name)
-                          or db:r1(pagecount_sql, pagesize)
+  local recent_tbl, pagecount
+  if name then
+    recent_tbl = db:q(sql, name, pagesize, (page - 1) * pagesize)
+    pagecount = db:r1(pagecount_sql, pagesize, name)
+  else
+    recent_tbl = db:q(sql, pagesize, (page - 1) * pagesize)
+    pagecount = db:r1(pagecount_sql, pagesize)
+  end
 
   for i = 1, #recent_tbl do
     recent_tbl[i]["Files"] = pico.file.list(recent_tbl[i]["Board"], recent_tbl[i]["Number"])

commit 0352a79d0dae05535a9ba3f165075ae5817c047b

html.renderindex: add back thread terminating <hr />
Regression indroduced in 208b1d7b

pico.lua
@@ -638,6 +638,9 @@ function html.renderindex(index_tbl, overboard)
     end
 
     printf("</div>")
+    if i ~= #index_tbl then
+      printf("<hr />")
+    end
   end
 end
 