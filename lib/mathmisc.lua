neeshy/picochan

commit 44b0e2ad6137d9c4987171b97be2327ddce13d03

argon2.lua: minor cleanup

lib/mathmisc.lua
@@ -5,7 +5,7 @@ local ffi = require("ffi")
 function math.csrandom(min, max)
   local random_data = math.urandom(8)
   local n = ffi.cast("uint64_t *", ffi.new("uint8_t[8]", random_data))[0]
-  return tonumber(n % (max - min + 1)) + min
+  return tonumber((n % (max - min + 1)) + min)
 end
 
 function math.urandom(n)

lib/argon2.lua
@@ -5,18 +5,18 @@ local ffi = require("ffi")
       ffi.argon2 = ffi.load("argon2")
 local argon2 = {}
 
--- this is the replacement to the enum "argon2_type" in argon2.h
-argon2.type_d = 0
-argon2.type_i = 1
-argon2.type_id = 2
-
--- argon2_version
-argon2.version_10 = 0x10
-argon2.version_13 = 0x13
-argon2.version = argon2.version_13
-
 ffi.cdef[[
-  typedef int argon2_type;
+  typedef enum Argon2_type {
+    Argon2_d = 0,
+    Argon2_i = 1,
+    Argon2_id = 2
+  } argon2_type;
+
+  typedef enum Argon2_version {
+    ARGON2_VERSION_10 = 0x10,
+    ARGON2_VERSION_13 = 0x13,
+    ARGON2_VERSION_NUMBER = ARGON2_VERSION_13
+  } argon2_version;
 
   size_t argon2_encodedlen(uint32_t t_cost, uint32_t m_cost,
                            uint32_t parallelism, uint32_t saltlen,
@@ -33,77 +33,42 @@ ffi.cdef[[
 ]]
 
 local hashtype_lut = {
-  argon2d = 0,
-  argon2i = 1,
-  argon2id = 2,
-}
-
-local errcode_lut = {
-  [0] = "(0) ARGON2_OK",
-  [-1] = "(-1) ARGON2_OUTPUT_PTR_NULL",
-  [-2] = "(-2) ARGON2_OUTPUT_TOO_SHORT",
-  [-3] = "(-3) ARGON2_OUTPUT_TOO_LONG",
-  [-4] = "(-4) ARGON2_PWD_TOO_SHORT",
-  [-5] = "(-5) ARGON2_PWD_TOO_LONG",
-  [-6] = "(-6) ARGON2_SALT_TOO_SHORT",
-  [-7] = "(-7) ARGON2_SALT_TOO_LONG",
-  [-8] = "(-8) ARGON2_AD_TOO_SHORT",
-  [-9] = "(-9) ARGON2_AD_TOO_LONG",
-  [-10] = "(-10) ARGON2_SECRET_TOO_SHORT",
-  [-11] = "(-11) ARGON2_SECRET_TOO_LONG",
-  [-12] = "(-12) ARGON2_TIME_TOO_SMALL",
-  [-13] = "(-13) ARGON2_TIME_TOO_LARGE",
-  [-14] = "(-14) ARGON2_MEMORY_TOO_LITTLE",
-  [-15] = "(-15) ARGON2_MEMORY_TOO_MUCH",
-  [-16] = "(-16) ARGON2_LANES_TOO_FEW",
-  [-17] = "(-17) ARGON2_LANES_TOO_MANY",
-  [-18] = "(-18) ARGON2_PWD_PTR_MISMATCH",
-  [-19] = "(-19) ARGON2_SALT_PTR_MISMATCH",
-  [-20] = "(-20) ARGON2_SECRET_PTR_MISMATCH",
-  [-21] = "(-21) ARGON2_AD_PTR_MISMATCH",
-  [-22] = "(-22) ARGON2_MEMORY_ALLOCATION_ERROR",
-  [-23] = "(-23) ARGON2_FREE_MEMORY_CBK_NULL",
-  [-24] = "(-24) ARGON2_ALLOCATE_MEMORY_CBK_NULL",
-  [-25] = "(-25) ARGON2_INCORRECT_PARAMETER",
-  [-26] = "(-26) ARGON2_INCORRECT_TYPE",
-  [-27] = "(-27) ARGON2_OUT_PTR_MISMATCH",
-  [-28] = "(-28) ARGON2_THREADS_TOO_FEW",
-  [-29] = "(-29) ARGON2_THREADS_TOO_MANY",
-  [-30] = "(-30) ARGON2_MISSING_ARGS",
-  [-31] = "(-31) ARGON2_ENCODING_FAIL",
-  [-32] = "(-32) ARGON2_DECODING_FAIL",
-  [-33] = "(-33) ARGON2_THREAD_FAIL",
-  [-34] = "(-34) ARGON2_DECODING_LENGTH_FAIL",
-  [-35] = "(-35) ARGON2_VERIFY_MISMATCH"
+  argon2d = ffi.argon2.Argon2_d,
+  argon2i = ffi.argon2.Argon2_i,
+  argon2id = ffi.argon2.Argon2_id,
 }
 
 -- only the password parameter is mandatory, the others are optional and will have
 -- sensible defaults set if they are not provided
 function argon2.digest(password, argon2_type, salt, params)
   assert(type(password) == "string", "incorrect datatype for parameter 'password'")
-  assert(argon2_type == nil or hashtype_lut[argon2_type] ~= nil, "invalid value for parameter 'argon2_type'")
-  assert(salt == nil or type(salt) == "string", "invalid datatype for parameter 'salt'")
-  assert(params == nil or type(params) == "table", "invalid datatype for parameter 'params'")
+  assert(argon2_type == nil or hashtype_lut[argon2_type], "invalid value for parameter 'argon2_type'")
+  assert(type(salt) == "string" or salt == nil, "invalid datatype for parameter 'salt'")
+  assert(type(params) == "table" or params == nil, "invalid datatype for parameter 'params'")
 
-  argon2_type = argon2_type or "argon2id"
-  salt = salt or string.random(16)
   params = params or {}
+  assert(type(params.t_cost) == "number" or params.t_cost == nil, "invalid datatype for parameter 'params.t_cost'")
+  assert(type(params.m_cost) == "number" or params.m_cost == nil, "invalid datatype for parameter 'params.m_cost'")
+  assert(type(params.parallelism) == "number" or params.parallelism == nil, "invalid datatype for parameter 'params.parallelism'")
+  assert(type(params.hashlen) == "number" or params.hashlen == nil, "invalid datatype for parameter 'params.hashlen'")
+  assert(type(params.version) == "number" or params.version == nil, "invalid datatype for parameter 'params.version'")
+
+  argon2_type = argon2_type and hashtype_lut[argon2_type] or ffi.argon2.Argon2_id
+  salt = salt or string.random(16)
   params.t_cost = params.t_cost or 16
   params.m_cost = params.m_cost or 2^16
   params.parallelism = params.parallelism or 4
-  params.version = params.version or argon2.version
+  params.hashlen = params.hashlen or 64
+  params.version = params.version or ffi.argon2.ARGON2_VERSION_NUMBER
 
-  local errcode, result, resultlen
-
-  resultlen = ffi.argon2.argon2_encodedlen(params.t_cost, params.m_cost, params.parallelism,
-                                           #salt, 64, hashtype_lut[argon2_type])
-  result = ffi.new("char[?]", resultlen)
-  errcode = ffi.argon2.argon2_hash(params.t_cost, params.m_cost, params.parallelism,
-                                   password, #password, salt, #salt, nil, 64,
-                                   result, resultlen, hashtype_lut[argon2_type], params.version)
-
+  local resultlen = ffi.argon2.argon2_encodedlen(params.t_cost, params.m_cost, params.parallelism,
+                                                 #salt, params.hashlen, argon2_type)
+  local result = ffi.new("char[?]", resultlen)
+  local errcode = ffi.argon2.argon2_hash(params.t_cost, params.m_cost, params.parallelism,
+                                         password, #password, salt, #salt, nil, params.hashlen,
+                                         result, resultlen, argon2_type, params.version)
   if errcode ~= 0 then
-    return nil, errcode_lut[errcode]
+    return nil, errcode
   end
   return ffi.string(result, resultlen)
 end
@@ -111,11 +76,14 @@ end
 function argon2.verify(password, hash, argon2_type)
   assert(type(password) == "string", "incorrect datatype for parameter 'password'")
   assert(type(hash) == "string", "incorrect datatype for parameter 'hash'")
-  assert(argon2_type == nil or hashtype_lut[argon2_type] ~= nil, "invalid value for parameter 'argon2_type'")
+  assert(argon2_type == nil or hashtype_lut[argon2_type], "invalid value for parameter 'argon2_type'")
 
-  argon2_type = argon2_type or "argon2id"
-  local errcode = ffi.argon2.argon2_verify(hash, password, #password, hashtype_lut[argon2_type])
-  return errcode == 0, errcode_lut[errcode]
+  argon2_type = argon2_type and hashtype_lut[argon2_type] or ffi.argon2.Argon2_id
+  local errcode = ffi.argon2.argon2_verify(hash, password, #password, argon2_type)
+  if errcode ~= 0 then
+    return false, errcode
+  end
+  return true
 end
 
 return argon2

lib/iomisc.lua
@@ -2,7 +2,6 @@
 
 function io.fileexists(path)
   local f = io.open(path, "r")
-
   if f ~= nil then
     f:close()
     return true

lib/openbsd.lua
@@ -14,7 +14,6 @@ function openbsd.pledge(promises, execpromises)
   assert(type(execpromises) == "string" or execpromises == nil, "incorrect datatype for parameter 'execpromises'")
 
   local retval = ffi.C.pledge(promises, execpromises)
-
   if retval == -1 then
     return false, ffi.string(ffi.C.strerror(ffi.errno()))
   end
@@ -26,7 +25,6 @@ function openbsd.unveil(path, permissions)
   assert(type(permissions) == "string" or permissions == nil, "incorrect datatype for parameter 'permissions'")
 
   local retval = ffi.C.unveil(path, permissions)
-
   if retval == -1 then
     return false, ffi.string(ffi.C.strerror(ffi.errno()))
   end

lib/sha.lua
@@ -29,21 +29,16 @@ local hashlen_lut = {
 }
 
 local function hex(data)
-  local byte = string.byte
-  local sub = string.sub
   local result = {}
-
   for i = 1, #data do
-    result[#result + 1] = string.format("%02x", byte(sub(data, i, i)))
+    result[#result + 1] = string.format("%02x", string.byte(string.sub(data, i, i)))
   end
-
   return table.concat(result)
 end
 
 function sha.hash(hashtype, data)
-  assert(type(hashtype) == "string", "incorrect datatype for parameter 'hashtype'")
   assert(type(data) == "string", "incorrect datatype for parameter 'data'")
-  local hashfunc = assert(hashfunc_lut[hashtype], "invalid hash type")
+  local hashfunc = assert(hashfunc_lut[hashtype], "incorrect value for parameter 'hashtype'")
   return hex(ffi.string(hashfunc(data, #data, nil), hashlen_lut[hashtype]))
 end
 

commit a8dfc79eaad17d4bef5e16a769dededf9464f12b

math.csrandom: fix regression introduced in 44b0e2ad

lib/mathmisc.lua
@@ -5,7 +5,7 @@ local ffi = require("ffi")
 function math.csrandom(min, max)
   local random_data = math.urandom(8)
   local n = ffi.cast("uint64_t *", ffi.new("uint8_t[8]", random_data))[0]
-  return tonumber((n % (max - min + 1)) + min)
+  return tonumber(n % (max - min + 1)) + min
 end
 
 function math.urandom(n)
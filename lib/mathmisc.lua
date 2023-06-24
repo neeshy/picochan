a8dfc79eaad17d4bef5e16a769dededf9464f12b
neeshy <neeshy@tfwno.gf>
2023-06-24 00:23:38 -0400

    math.csrandom: fix regression introduced in 44b0e2ad

--- lib/mathmisc.lua
+++ lib/mathmisc.lua
@@ -5,7 +5,7 @@ local ffi = require("ffi")
 function math.csrandom(min, max)
   local random_data = math.urandom(8)
   local n = ffi.cast("uint64_t *", ffi.new("uint8_t[8]", random_data))[0]
-  return tonumber((n % (max - min + 1)) + min)
+  return tonumber(n % (max - min + 1)) + min
 end
 
 function math.urandom(n)
